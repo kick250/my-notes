dd6a736005a549b62567237fbef1b011c95be064
kick250 <kicklobal@gmail.com>
2022-09-17 20:34:57 -0300

    comecar projeto

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,21 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <router-link class="navbar-brand" to="/">Home</router-link>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item active">
+            <router-link class="nav-link" to="/">Adicionar</router-link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+    <router-view/>
+  </div>
 </template>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
 
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/store/index.js
@@ -0,0 +1,17 @@
+import { createStore } from 'vuex'
+
+export default createStore({
+  state: {
+    notes: [
+      {title: 'trabalhos', content: 'TP1,TP2,TP3,TP4'},
+      {title: 'trabalhos', content: 'TP1,TP2,TP3,TP4'},
+      {title: 'trabalhos', content: 'TP1,TP2,TP3,TP4'},
+      {title: 'trabalhos', content: 'TP1,TP2,TP3,TP4'},
+      {title: 'trabalhos', content: 'TP1,TP2,TP3,TP4'}
+    ]
+  },
+  mutations: {
+  },
+  actions: {
+  }
+})
